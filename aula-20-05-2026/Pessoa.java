b4e41d648da9d42a00e46bd0d031ff08e16ace5e
ranyelsoncarvalho1 <ranyelson.carvalho@undf.edu.br>
2026-05-20 11:14:07 -0300

    Create Pessoa.java

---
+++ aula-20-05-2026/Pessoa.java
@@ -0,0 +1,50 @@
+package heranca;
+
+public class Pessoa {
+
+	//atributos
+	protected String nome;
+	protected int idade;
+	
+	//construtor
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	//getters e setters (modificadores)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	
+	//métodos (comportamentos)
+	public void apresentar() {
+		System.out.println("Nome: " + nome + " Idade: " + idade);
+	}
+	
+	@Override
+	public String toString() {
+		return "Nome: " + nome + 
+				" Idade: " + idade;
+	}
+	
+	//método que não pode ser sobreescrito
+	public final void mostrarDetalhes() {
+		System.out.println("Nome: " + nome);
+	}
+	
+	
+}
