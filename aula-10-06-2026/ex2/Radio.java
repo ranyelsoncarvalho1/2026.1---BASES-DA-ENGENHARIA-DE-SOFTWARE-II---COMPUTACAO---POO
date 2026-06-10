3fe6fd1cf4b835e1c5727e885280429b772f4b04
ranyelsoncarvalho1 <ranyelson.carvalho@undf.edu.br>
2026-06-10 09:06:37 -0300

    Create Radio.java

---
+++ aula-10-06-2026/ex2/Radio.java
@@ -0,0 +1,7 @@
+package interfaces_2;
+
+public interface Radio {
+    void ligar();             // Método para ligar o rádio
+    void desligar();          // Método para desligar o rádio
+    void trocarEstacao(double novaEstacao); // Método para trocar a estação
+}
