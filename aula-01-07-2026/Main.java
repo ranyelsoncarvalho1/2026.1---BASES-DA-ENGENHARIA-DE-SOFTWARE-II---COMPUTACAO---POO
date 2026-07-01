a16ad7fd20f1795239bbaf260a311d7eade42cb9
ranyelsoncarvalho1 <ranyelson.carvalho@undf.edu.br>
2026-07-01 11:53:35 -0300

    Create Main.java

---
+++ aula-01-07-2026/Main.java
@@ -0,0 +1,11 @@
+package interfaceGrafica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TelaCadastroAluno tela = new TelaCadastroAluno();
+		tela.setVisible(true);
+	}
+
+}
