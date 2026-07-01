623e49a14b3966d27ad86fb54559063f6582c806
ranyelsoncarvalho1 <ranyelson.carvalho@undf.edu.br>
2026-07-01 15:50:20 -0300

    Atualização do modelo, com as opções de editar, excluir e limpar.

--- aula-01-07-2026/TelaCadastroAluno.java
+++ aula-01-07-2026/TelaCadastroAluno.java
@@ -38,13 +38,13 @@ public class TelaCadastroAluno extends JFrame {
 		txtCurso.setBounds(90, 60, 220, 25); //posição
 		getContentPane().add(txtCurso);
 		
-		//botão
+		//botões
 		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setBounds(322, 18, 120, 30);
+		btnSalvar.setBounds(319, 18, 120, 30);
 		getContentPane().add(btnSalvar);
 		
 		JButton btnEditar = new JButton("Editar");
-		btnEditar.setBounds(332, 59, 117, 29);
+		btnEditar.setBounds(322, 59, 117, 29);
 		getContentPane().add(btnEditar);
 		
 		modelo = new DefaultTableModel(); //criar o modelo que vai armazenar os dados
@@ -57,15 +57,30 @@ public class TelaCadastroAluno extends JFrame {
 		
 		JScrollPane scroll = new JScrollPane(table);
 		scroll.setBounds(20, 120, 500, 250);
-		add(scroll);
+		getContentPane().add(scroll);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(451, 19, 117, 29);
+		getContentPane().add(btnExcluir);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(451, 59, 117, 29);
+		getContentPane().add(btnLimpar);
+		
+		JLabel lblNewLabel = new JLabel("UnDF - Engenharia de Software - POO");
+		lblNewLabel.setBounds(201, 382, 257, 16);
+		getContentPane().add(lblNewLabel);
 		
         
-		//ação quando o botão for clicado
+		//ações quando os botões são clicados
 		btnSalvar.addActionListener(e -> salvarAluno());
 		btnEditar.addActionListener(e -> editarAluno());
+		btnExcluir.addActionListener(e -> excluirAluno());
+		btnLimpar.addActionListener(e -> limparCampos());
 		
 	}
 	
+	//metodo para salvar aluno com validação
 	private void salvarAluno() {
 		//JOptionPane.showMessageDialog(null, "Teste");
 		String nome = txtNome.getText(); //pega o texto digitado em nome
@@ -84,18 +99,53 @@ public class TelaCadastroAluno extends JFrame {
 					aluno.getNome(), //pega os itens digitados
 					aluno.getCurso()
 			});
+			JOptionPane.showMessageDialog(this, "Cadastro realizado com sucesso!");
 		} else {
 			modelo.setValueAt(aluno.getNome(), linhaSelecionada, 0);
 			modelo.setValueAt(aluno.getCurso(), linhaSelecionada, 1);
+			JOptionPane.showMessageDialog(this, "Aluno atualizado com sucesso!");
 			linhaSelecionada = -1; //voltando para o modo de cadastro
 		}
-		
+		limparCampos();
 	}
 	
+	//método para editar um aluno com validação
 	private void editarAluno() {
 		//JOptionPane.showMessageDialog(null, "Teste");
 		linhaSelecionada = table.getSelectedRow();
+		
+		//validação para verificar se tem algum aluno selecionado
+		if (linhaSelecionada == -1) {
+			JOptionPane.showMessageDialog(this, "Selecione um aluno.");
+            return;
+		}
+		
 		txtNome.setText(modelo.getValueAt(linhaSelecionada, 0).toString());
 		txtCurso.setText(modelo.getValueAt(linhaSelecionada, 1).toString());
+	}
+	
+	//metodo para limpar os dados
+	private void limparCampos() {
+
+        txtNome.setText(""); 
+        txtCurso.setText(""); 
+
+        txtNome.requestFocus(); //fazer o curso voltar ao ponto de origem do campo
+    }
+	
+	//metodo de exclusão
+	private void excluirAluno() {
+		int linha = table.getSelectedRow();
+		if (linha == -1) {
+            JOptionPane.showMessageDialog(this, "Selecione um aluno.");
+            return;
+        }
+		
+		int opcao = JOptionPane.showConfirmDialog(this,"Deseja excluir este aluno?", "Confirmação", JOptionPane.YES_NO_OPTION);
+		if (opcao == JOptionPane.YES_OPTION) {
+			modelo.removeRow(linha);
+			limparCampos();
+			linhaSelecionada = -1;
+		}
 	}
 }
